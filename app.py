e26f18592c81a6f804c54a2d914df39a4e946fc2
Prateek <prateeksapple@gmail.com>
2015-06-05 10:37:52 +0530

    Check 7

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def get_tasks():
     return jsonify(comicJSON)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
